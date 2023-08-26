18b44b1047729d06eb7cee85ff183416afbac27e
Varun Sahni <66314478+notskamr@users.noreply.github.com>
2023-08-26 11:05:43 +0530

    removed admin

--- .eleventy.js
+++ .eleventy.js
@@ -26,7 +26,6 @@ module.exports = (eleventyConfig) => {
 
   eleventyConfig.addPassthroughCopy("src/scripts/toggle_theme.js");
   eleventyConfig.addPassthroughCopy("src/assets", "/assets");
-  eleventyConfig.addPassthroughCopy("src/admin", "/admin");
   eleventyConfig.addWatchTarget("styles/**/*.css");
   return {
     dir: {
