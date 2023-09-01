8b6ed7f2fc2cc86d193c7de31cebeffff6453f98
Varun Sahni <66314478+notskamr@users.noreply.github.com>
2023-09-01 16:13:23 +0530

    redefault toc option

--- .eleventy.js
+++ .eleventy.js
@@ -25,14 +25,7 @@ module.exports = (eleventyConfig) => {
       );
   });
 
-  eleventyConfig.addPlugin(require("eleventy-plugin-nesting-toc"), {
-    tags: ["h2", "h3", "h4"], // Which heading tags are selected (headings must each have an ID attribute)
-    ignoredElements: [], // Elements to ignore when constructing the label for every header (useful for ignoring permalinks, must be selectors)
-    wrapper: "nav", // Element to put around the root `ol`
-    wrapperClass: "toc", // Class for the element around the root `ol`
-    headingText: "", // Optional text to show in heading above the wrapper element
-    headingTag: "h2", // Heading tag when showing heading above the wrapper element
-  });
+  eleventyConfig.addPlugin(require("eleventy-plugin-nesting-toc"));
 
   eleventyConfig.setLibrary("md", markdownLib);
 
