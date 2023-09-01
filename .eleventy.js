c1401267a63a3d63e82001b8021264f6b7be55e2
Varun Sahni <66314478+notskamr@users.noreply.github.com>
2023-09-01 16:12:22 +0530

    more changes

--- .eleventy.js
+++ .eleventy.js
@@ -25,13 +25,20 @@ module.exports = (eleventyConfig) => {
       );
   });
 
-  eleventyConfig.addPlugin(require("eleventy-plugin-nesting-toc"));
+  eleventyConfig.addPlugin(require("eleventy-plugin-nesting-toc"), {
+    tags: ["h2", "h3", "h4"], // Which heading tags are selected (headings must each have an ID attribute)
+    ignoredElements: [], // Elements to ignore when constructing the label for every header (useful for ignoring permalinks, must be selectors)
+    wrapper: "nav", // Element to put around the root `ol`
+    wrapperClass: "toc", // Class for the element around the root `ol`
+    headingText: "", // Optional text to show in heading above the wrapper element
+    headingTag: "h2", // Heading tag when showing heading above the wrapper element
+  });
 
   eleventyConfig.setLibrary("md", markdownLib);
 
   eleventyConfig.addPassthroughCopy("src/scripts/toggle_theme.js");
   eleventyConfig.addPassthroughCopy("src/assets", "/assets");
-  eleventyConfig.addWatchTarget("styles/**/*.css");
+  eleventyConfig.addWatchTarget("src/styles");
   return {
     dir: {
       input: "src",
